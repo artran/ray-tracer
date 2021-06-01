afc63e147a1114b4b289676914e1d98bf0a3c201
Ray Tran <ray@artran.co.uk>
2021-06-01 21:03:45 +0100

    Tidy code

--- src/transform.rs
+++ src/transform.rs
@@ -7,10 +7,7 @@ pub trait Transform {
 impl Transform for Matrix4<f32> {
     fn translation(x: f32, y: f32, z: f32) -> Matrix4<f32> {
         Matrix4::new(
-            1.0, 0.0, 0.0, x,
-            0.0, 1.0, 0.0, y,
-            0.0, 0.0, 1.0, z,
-            0.0, 0.0, 0.0, 1.0,
+            1.0, 0.0, 0.0, x, 0.0, 1.0, 0.0, y, 0.0, 0.0, 1.0, z, 0.0, 0.0, 0.0, 1.0,
         )
     }
 }
@@ -38,18 +35,18 @@ mod tests {
 
     #[test]
     fn multiplying_by_the_inverse_of_a_translation_matrix() {
-       let transform = Matrix4::translation(5.0, -3.0, 2.0);
-       let inv = transform.try_inverse().unwrap();
-       let p = Vector4::point(-3.0, 4.0, 5.0);
+        let transform = Matrix4::translation(5.0, -3.0, 2.0);
+        let inv = transform.try_inverse().unwrap();
+        let p = Vector4::point(-3.0, 4.0, 5.0);
 
-       assert_that!(inv * p).is_equal_to(Vector4::point(-8.0, 7.0, 3.0));
+        assert_that!(inv * p).is_equal_to(Vector4::point(-8.0, 7.0, 3.0));
     }
 
     #[test]
-   fn translation_does_not_affect_vectors() {
-       let transform = Matrix4::translation(5.0, -3.0, 2.0);
-       let v = Vector4::vector(-3.0, 4.0, 5.0);
+    fn translation_does_not_affect_vectors() {
+        let transform = Matrix4::translation(5.0, -3.0, 2.0);
+        let v = Vector4::vector(-3.0, 4.0, 5.0);
 
-       assert_that!(transform * v).is_equal_to(v);
-   }
+        assert_that!(transform * v).is_equal_to(v);
+    }
 }
