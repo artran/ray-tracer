5f374873741877b012c1ee87eed67f37e2cdb3d8
Ray Tran <ray@artran.co.uk>
2021-07-02 17:47:46 +0100

    Implement view transform

--- src/transform.rs
+++ src/transform.rs
@@ -1,4 +1,4 @@
-use nalgebra::Matrix4;
+use nalgebra::{Matrix4, Vector4};
 
 pub trait Transform {
     fn translation(x: f32, y: f32, z: f32) -> Matrix4<f32>;
@@ -7,6 +7,7 @@ pub trait Transform {
     fn rotation_y(r: f32) -> Matrix4<f32>;
     fn rotation_z(r: f32) -> Matrix4<f32>;
     fn shearing(xy: f32, xz: f32, yx: f32, yz: f32, zx: f32, zy: f32) -> Matrix4<f32>;
+    fn view_transform(from: Vector4<f32>, to: Vector4<f32>, up: Vector4<f32>) -> Matrix4<f32>;
 }
 
 impl Transform for Matrix4<f32> {
@@ -56,12 +57,27 @@ impl Transform for Matrix4<f32> {
     }
 
     fn shearing(xy: f32, xz: f32, yx: f32, yz: f32, zx: f32, zy: f32) -> Matrix4<f32> {
-        Matrix4::new (
+        Matrix4::new(
             1.0, xy, xz, 0.0,
             yx, 1.0, yz, 0.0,
             zx, zy, 1.0, 0.0,
             0.0, 0.0, 0.0, 1.0,
         )
+    }
+
+    fn view_transform(from: Vector4<f32>, to: Vector4<f32>, up: Vector4<f32>) -> Matrix4<f32> {
+        let forward = ((to - from).normalize()).remove_row(3);
+        let upn = (up.normalize()).remove_row(3);
+        let left = forward.cross(&upn);
+        let true_up = left.cross(&forward);
+        let orientation = Matrix4::new(
+            left.x, left.y, left.z, 0.0,
+            true_up.x, true_up.y, true_up.z, 0.0,
+            -forward.x, -forward.y, -forward.z, 0.0,
+            0.0, 0.0, 0.0, 1.0,
+        );
+
+        orientation * Matrix4::translation(-from.x, -from.y, -from.z)
     }
 }
 
@@ -116,7 +132,7 @@ mod tests {
     #[test]
     fn scaling_matrix_applied_to_a_point() {
         let transform = Matrix4::scaling(2.0, 3.0, 4.0);
-        let  p = Vector4::point(-4.0, 6.0, 8.0);
+        let p = Vector4::point(-4.0, 6.0, 8.0);
 
         assert_that!(transform * p).is_equal_to(Vector4::point(-8.0, 18.0, 32.0));
     }
@@ -149,39 +165,39 @@ mod tests {
     #[test]
     fn rotating_a_point_around_the_x_axis() {
         let p = Vector4::point(0.0, 1.0, 0.0);
-        let half_quarter = Matrix4::rotation_x(PI/4.0);
-        let full_quarter = Matrix4::rotation_x(PI/2.0);
+        let half_quarter = Matrix4::rotation_x(PI / 4.0);
+        let full_quarter = Matrix4::rotation_x(PI / 2.0);
 
-        vector_values_are_close(half_quarter * p, Vector4::point(0.0, 2.0_f32.sqrt()/2.0, 2.0_f32.sqrt()/2.0), 0.0001);
+        vector_values_are_close(half_quarter * p, Vector4::point(0.0, 2.0_f32.sqrt() / 2.0, 2.0_f32.sqrt() / 2.0), 0.0001);
         vector_values_are_close(full_quarter * p, Vector4::point(0.0, 0.0, 1.0), 0.00001);
     }
 
     #[test]
     fn the_inverse_of_an_x_rotation_rotates_in_the_opposite_direction() {
         let p = Vector4::point(0.0, 1.0, 0.0);
-        let half_quarter = Matrix4::rotation_x(PI/4.0);
+        let half_quarter = Matrix4::rotation_x(PI / 4.0);
         let inv = half_quarter.try_inverse().unwrap();
 
-        vector_values_are_close(inv * p, Vector4::point(0.0, 2.0_f32.sqrt()/2.0, -2.0_f32.sqrt()/2.0), 0.00001);
+        vector_values_are_close(inv * p, Vector4::point(0.0, 2.0_f32.sqrt() / 2.0, -2.0_f32.sqrt() / 2.0), 0.00001);
     }
 
     #[test]
     fn rotating_a_point_around_the_y_axis() {
         let p = Vector4::point(0.0, 0.0, 1.0);
-        let half_quarter = Matrix4::rotation_y(PI/4.0);
-        let full_quarter = Matrix4::rotation_y(PI/2.0);
+        let half_quarter = Matrix4::rotation_y(PI / 4.0);
+        let full_quarter = Matrix4::rotation_y(PI / 2.0);
 
-        vector_values_are_close(half_quarter * p, Vector4::point(2.0_f32.sqrt()/2.0, 0.0, 2.0_f32.sqrt()/2.0), 0.00001);
+        vector_values_are_close(half_quarter * p, Vector4::point(2.0_f32.sqrt() / 2.0, 0.0, 2.0_f32.sqrt() / 2.0), 0.00001);
         vector_values_are_close(full_quarter * p, Vector4::point(1.0, 0.0, 0.0), 0.00001);
     }
 
     #[test]
     fn rotating_a_point_around_the_z_axis() {
         let p = Vector4::point(0.0, 1.0, 0.0);
-        let half_quarter = Matrix4::rotation_z(PI/4.0);
-        let full_quarter = Matrix4::rotation_z(PI/2.0);
+        let half_quarter = Matrix4::rotation_z(PI / 4.0);
+        let full_quarter = Matrix4::rotation_z(PI / 2.0);
 
-        vector_values_are_close(half_quarter * p, Vector4::point(-2.0_f32.sqrt()/2.0, 2.0_f32.sqrt()/2.0, 0.0), 0.0001);
+        vector_values_are_close(half_quarter * p, Vector4::point(-2.0_f32.sqrt() / 2.0, 2.0_f32.sqrt() / 2.0, 0.0), 0.0001);
         vector_values_are_close(full_quarter * p, Vector4::point(-1.0, 0.0, 0.0), 0.0001);
     }
 
@@ -189,7 +205,7 @@ mod tests {
     fn a_shearing_transformation_moves_x_in_proportion_to_y() {
         let transform = Matrix4::shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
         let p = Vector4::point(2.0, 3.0, 4.0);
-      
+
         assert_that!(transform * p).is_equal_to(Vector4::point(5.0, 3.0, 4.0));
     }
 
@@ -197,7 +213,7 @@ mod tests {
     fn a_shearing_transformation_moves_x_in_proportion_to_z() {
         let transform = Matrix4::shearing(0.0, 1.0, 0.0, 0.0, 0.0, 0.0);
         let p = Vector4::point(2.0, 3.0, 4.0);
-      
+
         assert_that!(transform * p).is_equal_to(Vector4::point(6.0, 3.0, 4.0));
     }
 
@@ -205,7 +221,7 @@ mod tests {
     fn a_shearing_transformation_moves_y_in_proportion_to_x() {
         let transform = Matrix4::shearing(0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
         let p = Vector4::point(2.0, 3.0, 4.0);
-      
+
         assert_that!(transform * p).is_equal_to(Vector4::point(2.0, 5.0, 4.0));
     }
 
@@ -213,7 +229,7 @@ mod tests {
     fn a_shearing_transformation_moves_y_in_proportion_to_z() {
         let transform = Matrix4::shearing(0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
         let p = Vector4::point(2.0, 3.0, 4.0);
-      
+
         assert_that!(transform * p).is_equal_to(Vector4::point(2.0, 7.0, 4.0));
     }
 
@@ -221,7 +237,7 @@ mod tests {
     fn a_shearing_transformation_moves_z_in_proportion_to_x() {
         let transform = Matrix4::shearing(0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
         let p = Vector4::point(2.0, 3.0, 4.0);
-      
+
         assert_that!(transform * p).is_equal_to(Vector4::point(2.0, 3.0, 6.0));
     }
 
@@ -229,14 +245,14 @@ mod tests {
     fn a_shearing_transformation_moves_z_in_proportion_to_y() {
         let transform = Matrix4::shearing(0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
         let p = Vector4::point(2.0, 3.0, 4.0);
-      
+
         assert_that!(transform * p).is_equal_to(Vector4::point(2.0, 3.0, 7.0));
     }
 
     #[test]
     fn individual_transformations_are_applied_in_sequence() {
         let p = Vector4::point(1.0, 0.0, 1.0);
-        let a = Matrix4::rotation_x(PI/2.0);
+        let a = Matrix4::rotation_x(PI / 2.0);
         let b = Matrix4::scaling(5.0, 5.0, 5.0);
         let c = Matrix4::translation(10.0, 5.0, 7.0);
 
@@ -256,7 +272,7 @@ mod tests {
     #[test]
     fn chained_transformations_must_be_applied_in_reverse_order() {
         let p = Vector4::point(1.0, 0.0, 1.0);
-        let a = Matrix4::rotation_x(PI/2.0);
+        let a = Matrix4::rotation_x(PI / 2.0);
         let b = Matrix4::scaling(5.0, 5.0, 5.0);
         let c = Matrix4::translation(10.0, 5.0, 7.0);
 
@@ -265,52 +281,58 @@ mod tests {
         vector_values_are_close(t * p, Vector4::point(15.0, 0.0, 7.0), 0.00001);
     }
 
-//    #[test]
-//    fn the_transformation_matrix_for_the_default_orientation() {
-//        let from = Vector4::point(0.0, 0.0, 0.0);
-//        let to = Vector4::point(0.0, 0.0, -1.0);
-//        let up = Vector4::vector(0.0, 1.0, 0.0);
-//
-//        let t = view_transform(from, to, up);
-//
-//        assert_that!(t).is_equal_to(Matrix4::identity());
-//    }
-//
-//    #[test]
-//    fn a_view_transformation_matrix_looking_in_positive_z_direction() {
-//        let from = Vector4::point(0.0, 0.0, 0.0);
-//        let to = Vector4::point(0.0, 0.0, 1.0);
-//        let up = Vector4::vector(0.0, 1.0, 0.0);
-//
-//        let t = view_transform(from, to, up);
-//
-//        assert_that!(t).is_equal_to(Matrix4::scaling(-1.0, 1.0, -1.0));
-//    }
-//
-//    #[test]
-//    fn the_view_transformation_moves_the_world() {
-//        let from = Vector4::point(0.0, 0.0, 8.0);
-//        let to = Vector4::point(0.0, 0.0, 0.0);
-//        let up = Vector4::vector(0.0, 1.0, 0.0);
-//
-//        let t = view_transform(from, to, up);
-//
-//        assert_that!(t).is_equal_to(Matrix4::translation(0.0, 0.0, -8.0));
-//    }
-//
-//    #[test]
-//    fn an_arbitrary_view_transformation() {
-//        let from = Vector4::point(1.0, 3.0, 2.0);
-//        let to = Vector4::point(4.0, -2.0, 8.0);
-//        let up = Vector4::vector(1.0, 1.0, 0.0);
-//
-//        let t = view_transform(from, to, up);
-//
-//        assert_that!(t).is_equal_to(Matrix4::new(
-//            -0.50709, 0.50709, 0.67612, -2.36643,
-//            0.76772, 0.60609, 0.12122, -2.82843,
-//            -0.35857, 0.59761, -0.71714, 0.00000,
-//            0.00000, 0.00000, 0.00000, 1.00000,
-//       ));
-//    }
+    #[test]
+    fn the_transformation_matrix_for_the_default_orientation() {
+        let from = Vector4::point(0.0, 0.0, 0.0);
+        let to = Vector4::point(0.0, 0.0, -1.0);
+        let up = Vector4::vector(0.0, 1.0, 0.0);
+
+        let t = Matrix4::view_transform(from, to, up);
+
+        assert_that!(t).is_equal_to(Matrix4::identity());
+    }
+
+    #[test]
+    fn a_view_transformation_matrix_looking_in_positive_z_direction() {
+        let from = Vector4::point(0.0, 0.0, 0.0);
+        let to = Vector4::point(0.0, 0.0, 1.0);
+        let up = Vector4::vector(0.0, 1.0, 0.0);
+
+        let t = Matrix4::view_transform(from, to, up);
+
+        assert_that!(t).is_equal_to(Matrix4::scaling(-1.0, 1.0, -1.0));
+    }
+
+    #[test]
+    fn the_view_transformation_moves_the_world() {
+        let from = Vector4::point(0.0, 0.0, 8.0);
+        let to = Vector4::point(0.0, 0.0, 0.0);
+        let up = Vector4::vector(0.0, 1.0, 0.0);
+
+        let t = Matrix4::view_transform(from, to, up);
+
+        assert_that!(t).is_equal_to(Matrix4::translation(0.0, 0.0, -8.0));
+    }
+
+    #[test]
+    fn an_arbitrary_view_transformation() {
+        let from = Vector4::point(1.0, 3.0, 2.0);
+        let to = Vector4::point(4.0, -2.0, 8.0);
+        let up = Vector4::vector(1.0, 1.0, 0.0);
+
+        let t = Matrix4::view_transform(from, to, up);
+
+        let expected = Matrix4::new(
+            -0.50709, 0.50709, 0.67612, -2.36643,
+            0.76772, 0.60609, 0.12122, -2.82843,
+            -0.35857, 0.59761, -0.71714, 0.00000,
+            0.00000, 0.00000, 0.00000, 1.00000,
+        );
+
+        for c in 0..4 {
+            for r in 0..4 {
+                assert_that!(t[(r, c)]).is_close_to(expected[(r, c)], 0.0001);
+            }
+        }
+    }
 }
